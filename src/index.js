9d954ccbc002d36950563128ec7384b0e696cb32
Carlio Murta Ramalho <carlito.ramalho@bs2.com>
2020-01-16 19:54:28 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './style.scss';
+
+// SCROLL SMOOTH
+$(document).ready(function() {
+  // MASK PHONE
+  $('#phone').mask('(00)00000-0000');
+  $('a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 101
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+// HEADER
+document.body.onscroll = function() {
+  scrollHandle();
+};
+const header = document.getElementById('header');
+function scrollHandle() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    header.className = 'opaque';
+    header.classList.remove('translucent');
+  } else {
+    header.className = 'translucent';
+    header.classList.remove('opaque');
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  target: 'web',
+  // externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash(),
+    new CopyPlugin([{ from: 'src/assets', to: 'assets' }])
+  ],
+  devServer: {
+    contentBase: 'dist/dev-build',
+    inline: true,
+    port: 3000,
+    hot: true,
+    progress: true,
+    historyApiFallback: true,
+    watchContentBase: true
+  }
+};
